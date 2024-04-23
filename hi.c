b2210aee1566e4c9e7fc28549942c01d7b14ddc7
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-04-23 16:50:12 +0530

    hi

--- hi.c
+++ hi.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
+    string s = "HI!";
 
-    printf("%i%c%c \n",c1 ,c2, c3);
+    printf("%i %i %i %i \n",s[0], s[1], s[2],s[3]);
 }
 
